3b70f9a86e27254d2da6359191f2ef0e74d27de6
Asem Rashed <asemrashed002@gmail.com>
2025-12-09 15:13:48 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,157 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const admin = require("firebase-admin");
+const serviceAccount = require("./e-tuitionsbd-firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const verifyToken = async (req, res, next) => {
+  const token = req.headers?.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.decodedUser = decodedToken;
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+};
+
+const uri = process.env.DB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+client
+  .connect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log("E-TuitionsBD server running on port:", port);
+    });
+  })
+  .catch(err => console.log(err));
+
+const database = client.db("e-tuitionsbd");
+const usersCollection = database.collection("users");
+
+const tuitionsCollection = database.collection("tuitions");
+
+app.get("/", (req, res) => {
+  res.send("E-TuitionsBD Server is running");
+});
+
+// USERS
+app.post("/users", async (req, res) => {
+  const newUser = req.body;
+  const query = { email: newUser.email };
+  const existingUser = await usersCollection.findOne(query);
+  if (existingUser) {
+    return res.status(400).send({ message: "User already exists" });
+  }
+  const result = await usersCollection.insertOne(newUser);
+  res.send(result);
+});
+
+// ... users endpoints ...
+app.get("/users", async (req, res) => {
+  try {
+    const { email } = req.query;
+    const query = {};
+    if (email) {
+      query.email = email;
+    }
+    const result = await usersCollection.find(query).toArray();
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+app.get("/users/me", async (req, res) => {
+    const query = { email: req.query.email };
+    const result = await usersCollection.findOne(query);
+    res.send(result);
+});
+app.patch("/users/me", async (req, res) => {
+  try {
+    const {email} = req.query
+    const data = req.body;
+    const query = { email: email };
+    const result = await usersCollection.updateOne(query, { $set: data });
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    const { status } = req.body;
+    const result = await usersCollection.updateOne(query, { $set: { status } });
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    const result = await usersCollection.deleteOne(query);
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// TUITIONS
+app.post("/tuitions", async (req, res) => {
+    try {
+        const item = req.body;
+        const result = await tuitionsCollection.insertOne(item);
+        res.send(result);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.get("/tuitions", async (req, res) => {
+    try {
+        let query = {};
+        if (req.query.email) {
+            query = { tutorEmail: req.query.email };
+        }
+        const result = await tuitionsCollection.find(query).toArray();
+        res.send(result);
+    } catch (err) {
+        console.log(err);
+    }
+});
+app.get("/latest-tuitions", async (req, res) => {
+    try {
+        const limit = parseInt(req.query.limit);
+        const result = await tuitionsCollection.find().sort({ createdAt: -1 }).limit(limit).toArray();
+        res.send(result);
+    } catch (err) {
+        console.log(err);
+    }
+});
